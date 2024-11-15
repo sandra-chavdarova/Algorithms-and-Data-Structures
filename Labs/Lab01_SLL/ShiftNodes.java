abf593709a34b4a111c585e70997f71a0b1514ee
Sandra Chavdarova <sandra.chavdarova@students.finki.ukim.mk>
2024-11-15 21:54:15 +0100

    Initial commit

---
+++ Labs/Lab01_SLL/ShiftNodes.java
@@ -0,0 +1,217 @@
+/*
+Дадена е еднострана поврзана листа чии што јазли содржат по еден String.
+Дополнително, даден е и уште еден природен број L.
+Во дадената листа потребно е сите јазли коишто содржат String со должина L да се поместат на крај на листата,
+задржувајќи го и оригиналниот распоред на елементите.
+
+Влез: Во првиот ред од влезот е даден бројот на елементи во листата - N,
+па во следните N редови се дадени самите елементи (String-ови) од листата.
+На крај, во последниот ред е даден бројот L.
+
+Излез: На излез треба да се испечати листата пред и после промената.
+
+/
+
+You are given a single linked list with String nodes.
+Additionally, you are given one more integer L.
+You need to move all nodes from the list that contain a String with exactly L characters to the end of the list,
+keeping the original order of elements as well.
+
+Input: In the first line from the input the number of elements in the list is given - N,
+and then in the next N lines the elements themselves.
+In the last line, the integer L is given.
+
+Output: The list before and after the transformation
+
+Input:
+4
+Apple
+Banana
+Pear
+Watermelon
+6
+
+Result:
+Apple->Banana->Pear->Watermelon
+Apple->Pear->Watermelon->Banana
+*/
+
+package Labs.Lab01_SLL;
+
+import java.util.Scanner;
+
+class SLLNode<E> {
+    protected E element;
+    protected SLLNode<E> succ;
+
+    public SLLNode(E elem, SLLNode<E> succ) {
+        this.element = elem;
+        this.succ = succ;
+    }
+}
+
+class SLL<E> {
+    private SLLNode<E> first;
+
+    public SLL() {
+        // Construct an empty SLL
+        this.first = null;
+    }
+
+    public void deleteList() {
+        first = null;
+    }
+
+    public int size() {
+        int listSize = 0;
+        SLLNode<E> temp = first;
+        while (temp != null) {
+            listSize++;
+            temp = temp.succ;
+        }
+        return listSize;
+    }
+
+    @Override
+    public String toString() {
+        String ret = new String();
+        if (first != null) {
+            SLLNode<E> temp = first;
+            ret += temp.element;
+            while (temp.succ != null) {
+                temp = temp.succ;
+                ret += "->" + temp.element;
+            }
+        } else
+            ret = "Prazna lista!!!";
+        return ret;
+    }
+
+    public void insertFirst(E o) {
+        SLLNode<E> ins = new SLLNode<E>(o, null);
+        ins.succ = first;
+        // SLLNode<E> ins = new SLLNode<E>(o, first);
+        first = ins;
+    }
+
+    public void insertAfter(E o, SLLNode<E> node) {
+        if (node != null) {
+            SLLNode<E> ins = new SLLNode<E>(o, node.succ);
+            node.succ = ins;
+        } else {
+            System.out.println("Dadenot jazol e null");
+        }
+    }
+
+    public void insertBefore(E o, SLLNode<E> before) {
+        if (first != null) {
+            SLLNode<E> temp = first;
+            if (first == before) {
+                this.insertFirst(o);
+                return;
+            }
+            // ako first!=before
+            while (temp.succ != before && temp.succ != null)
+                temp = temp.succ;
+            if (temp.succ == before) {
+                temp.succ = new SLLNode<E>(o, before);
+                ;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+            }
+        } else {
+            System.out.println("Listata e prazna");
+        }
+    }
+
+    public void insertLast(E o) {
+        if (first != null) {
+            SLLNode<E> temp = first;
+            while (temp.succ != null)
+                temp = temp.succ;
+            temp.succ = new SLLNode<E>(o, null);
+        } else {
+            insertFirst(o);
+        }
+    }
+
+    public E deleteFirst() {
+        if (first != null) {
+            SLLNode<E> temp = first;
+            first = first.succ;
+            return temp.element;
+        } else {
+            System.out.println("Listata e prazna");
+            return null;
+        }
+    }
+
+    public E delete(SLLNode<E> node) {
+        if (first != null) {
+            SLLNode<E> temp = first;
+            if (first == node) {
+                return this.deleteFirst();
+            }
+            while (temp.succ != node && temp.succ.succ != null)
+                temp = temp.succ;
+            if (temp.succ == node) {
+                temp.succ = temp.succ.succ;
+                return node.element;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+                return null;
+            }
+        } else {
+            System.out.println("Listata e prazna");
+            return null;
+        }
+
+    }
+
+    public SLLNode<E> getFirst() {
+        return first;
+    }
+
+    public SLLNode<E> find(E o) {
+        if (first != null) {
+            SLLNode<E> temp = first;
+            while (!temp.element.equals(o) && temp.succ != null)
+                temp = temp.succ;
+            if (temp.element.equals(o)) {
+                return temp;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+            }
+        } else {
+            System.out.println("Listata e prazna");
+        }
+        return null;
+    }
+}
+
+public class ShiftNodes {
+    public static void shift(SLL<String> list, int L) {
+        SLLNode<String> temp = list.getFirst();
+        int n = list.size();
+        for (int i = 0; i < n && temp != null; i++) {
+            if (temp.element.length() == L) {
+                list.insertLast(temp.element);
+                list.delete(temp);
+            }
+            temp = temp.succ;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        SLL<String> list = new SLL<String>();
+        for (int i = 0; i < n; i++) {
+            list.insertLast(scanner.next());
+        }
+        int L = scanner.nextInt();
+        System.out.println(list.toString());
+        shift(list, L);
+        System.out.println(list.toString());
+    }
+}
