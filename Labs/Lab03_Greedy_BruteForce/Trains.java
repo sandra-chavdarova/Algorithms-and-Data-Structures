fc2ff3ac89b36193e1f16aa5b94bf6bea33b853f
Sandra Chavdarova <sandra.chavdarova@students.finki.ukim.mk>
2024-11-20 22:10:20 +0100

    Initial commit

--- Labs/Lab03_Greedy_BruteForce/Trains.java
+++ Labs/Lab03_Greedy_BruteForce/Trains.java
@@ -1,4 +1,14 @@
 /*
+Дадени се N возови со време на пристигање и поаѓање.
+Да се најде минималниот број на платформи потребен за да се сместат тие возови.
+
+Влез: Првиот број од влезот е бројот на возови N,
+а потоа во следниот ред минутата на пристигнување и минутата на поаѓање на возот.
+
+Излез: Најмалиот потребен број на платформи за да се сместат сите возови
+
+/
+
 We are given N trains. Write an algorithm that finds the minimum number of platforms needed to schedule those trains.
 
 Input: the first number in the input is the number of trains N,
@@ -19,6 +29,9 @@ public class Trains {
                     int temp = arrivals[i];
                     arrivals[i] = arrivals[j];
                     arrivals[j] = temp;
+                    temp = departures[i];
+                    departures[i] = departures[j];
+                    departures[j] = temp;
                 }
                 if (departures[i] > departures[j]) {
                     int temp = departures[i];
@@ -34,32 +47,27 @@ public class Trains {
         int maxPlatforms = 0, platforms = 0;
         int a = 0, d = 0;
         while (a < n && d < n) {
-            if (arrivals[a] < departures[d]) {
+            if (arrivals[a] <= departures[d]) {
                 platforms++;
-                maxPlatforms = Math.max(maxPlatforms, platforms);
                 a++;
-            }
-            if (arrivals[a] > departures[d]) {
+            } else {
                 platforms--;
-                maxPlatforms = Math.max(maxPlatforms, platforms);
                 d++;
             }
+            maxPlatforms = Math.max(maxPlatforms, platforms);
         }
         return maxPlatforms;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int n = scanner.nextInt();
         int[] arrivals = new int[n];
         int[] departures = new int[n];
-
         for (int i = 0; i < n; i++) {
             arrivals[i] = scanner.nextInt();
             departures[i] = scanner.nextInt();
         }
-
         int minPlatforms = minPlatforms(arrivals, departures, n);
         System.out.println(minPlatforms);
     }
