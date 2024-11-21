573211a65940872456032f26a5c9311dd37898de
Sandra Chavdarova <sandra.chavdarova@students.finki.ukim.mk>
2024-11-21 14:35:59 +0100

    Initial commit

--- Labs/Lab04_DivideAndConquer_DynamicProgramming/MaxProduct.java
+++ Labs/Lab04_DivideAndConquer_DynamicProgramming/MaxProduct.java
@@ -1,10 +1,42 @@
 /*
-За дадена низа од броеви да се најде максималниот производ кој се формира
-со множење на броевите од некоја растечка подниза на таа низа.
+За дадена низа од броеви да се најде максималниот производ кој се формира со множење
+на броевите од некоја растечка подниза на таа низа.
 
 Влез: На влез во првиот ред е даден бројот на броеви во низата, а во вториот ред е дадена низата од броеви.
 
 Излез: На излез треба да се испечати максималниот производ кој се бара во задачата.
+
+Пример:
+Влез:
+6
+3 100 4 5 150 6
+
+Излез:
+45000
+
+Максималниот производ се формира од растечката подниза 3, 100, 150.
+Да се забележи дека најдолгата растечка подниза е друга, 3, 4, 5, 6.
+
+/
+
+For a given array of integers, find the maximum product
+formed by multiplication of the numbers of an increasing subarray of the given array.
+
+Input: The first line of the input contains N, the number of elements of the array.
+The second line of the input contains the array.
+
+Output: The maximum product formed in the described way.
+
+Example:
+Input:
+6
+3 100 4 5 150 6
+
+Output:
+45000
+
+The maximum product is formed from the increasing subarray 3, 100, 150.
+Note that the longest increasing subarray is 3, 4, 5, 6.
 
 Input:
 6
@@ -12,45 +44,41 @@ Input:
 
 Result:
 45000
-
-
-Input:
-8
-10 22 33 21 50 41 60 80
-
-Result:
-1742400000
 */
 
 package Labs.Lab04_DivideAndConquer_DynamicProgramming;
 
+import java.util.Scanner;
+
 public class MaxProduct {
-    static int max(int a, int b, int c) {
-        return Math.max(a, Math.max(b, c));
-    }
-
-    static int min(int a, int b, int c) {
-        return Math.min(a, Math.min(b, c));
-    }
-
     static int maxProduct(int[] arr) {
         int n = arr.length;
-        int max = arr[0];
-        int min = arr[0];
-        int result = arr[0];
+        int[] products = new int[n];
         for (int i = 1; i < n; i++) {
-            int temp = max(arr[i], arr[i] * max, arr[i] * min);
-            min = min(arr[i], arr[i] * max, arr[i] * min);
-            max = temp;
-            result = Math.max(result, max);
+            products[i] = arr[i];
         }
-
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i] && products[i] < products[j] * arr[i]) {
+                    products[i] = products[j] * arr[i];
+                }
+            }
+        }
+        int result = products[0];
+        for (int i = 1; i < n; i++) {
+            if (products[i] > result)
+                result = products[i];
+        }
         return result;
     }
 
     public static void main(String[] args) {
-        int[] arr = {-2, 6, -3, -10, 0, 2};
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
         System.out.println(maxProduct(arr));
     }
-
 }
