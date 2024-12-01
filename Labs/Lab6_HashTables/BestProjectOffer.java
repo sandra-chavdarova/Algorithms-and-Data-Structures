07060828f36329bb2873effbe9d9abbf8de05b49
Sandra Chavdarova <sandra.chavdarova@students.finki.ukim.mk>
2024-12-01 17:15:54 +0100

    Initial commit

--- Labs/Lab6_HashTables/BestProjectOffer.java
+++ Labs/Lab6_HashTables/BestProjectOffer.java
@@ -380,7 +380,7 @@ class Project {
     }
 }
 
-public class Solution {
+public class BestProjectOffer {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
