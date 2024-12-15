5e040180b9d77707c39f84cbf1e4a6567736db9f
Sandra Chavdarova <sandra.chavdarova@students.finki.ukim.mk>
2024-12-15 15:39:46 +0100

    Initial commit

---
+++ Leetcode/SameTree.java
@@ -0,0 +1,51 @@
+/*
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+Example
+       10
+      /  \
+     8    18
+
+       10
+      /  \
+     8    18
+
+Link: https://leetcode.com/problems/same-tree/description/
+*/
+
+package Leetcode;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+
+        this.val = val;
+
+        this.left = left;
+
+        this.right = right;
+
+    }
+}
+
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null || p.val != q.val)
+            return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
