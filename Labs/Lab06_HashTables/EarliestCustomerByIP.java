3ddd356b1143f2ebfe5972df49d4c90673a9d461
Sandra Chavdarova <sandra.chavdarova@students.finki.ukim.mk>
2025-01-04 13:55:02 +0100

    Initial commit

--- Labs/Lab06_HashTables/EarliestCustomerByIP.java
+++ Labs/Lab06_HashTables/EarliestCustomerByIP.java
@@ -1,3 +1,70 @@
+/*
+На влез во оваа задача ќе ви бидат дадени редови како следниот формат:
+Ime Prezime budzhet ip_adresa vreme grad cena
+Пример
+Jovan Todorov 1000 10.73.112.200 15:30 Bitola 760
+Кое што значи дека лицето со Име и Презиме Jovan Todorov, има буџет од 1000 денари,
+има IP адреса со мрежа 10.73.112 и домаќин (host number) 200,
+и се приклучил во 15:30 часот за да купи билет кон Bitola кој што чини 760 денари.
+
+Ќе ви бидат дадени N такви редови, по што ќе следи празен ред па уште M редови од истиот формат, кои ќе ги користиме за тестирање.
+
+Од редот за тестирање треба да ја извадите IP адресата на мрежата и потоа да го одговорите следното прашање со оваа мрежа:
+
+Од сите N лица на влез, кои што се поврзуваат со истата мрежа (од било кој домаќин во мрежата)
+колку од нив се вклучиле подоцна од 11:59;
+и од овие, кој од нив се вклучил најрано?
+
+(погледнете го тест примерот!)
+(доколку има повеќе со исто најмало време тогаш кој е првиот таков во влезот?) (секогаш ќе постои барем еден таков)
+
+Ова ќе треба да го направите за сите M редови за тестирање!
+
+Препорака, користете OBHT и/или CBHT.
+
+----------------------
+
+At the input of this task you will be given rows with the following format:
+First_name Last_name budget ip_address time city price
+Example
+Jovan Todorov 1000 10.73.112.200 15:30 Bitola 760
+Which means that the person with First name and Last name Jovan Todorov, has a budget of 1000 denars,
+ has an IP address with network 10.73.112 and host number 200,
+ and joined at 15:30 to buy a ticked to Bitola which costs 760 denars.
+
+You will be given N such rows, followed by an empty row and M more rows of the same format, which we will use for testing.
+
+From the test line you need to extract the IP address of the network and then answer the following question with this network:
+
+Of the N people at the input, from the ones who connect from the same network (from any host on the network)
+how many of them logged in later than 11:59;
+and of these, which one logged in the earliest?
+
+(if there are more with the same minimum time then which one is the first one in the input?)
+(there will always be at least one of them)
+
+You will need to do this for all M rows for testing!
+
+Recommendation, use OBHT and/or CBHT.
+
+Input:
+5
+Jovan Todorov    1000    10.73.112.200     16:30   Bitola     760
+Mitko Janev      4350    132.28.112.200    12:15   Krusevo    4000
+Sara Dobreva     2700    10.73.60.29       14:35   Bitola     2500
+Mence Trajanova  4000    10.73.112.112     11:25   Bitola     4200
+Viktor Jovev     2200    10.73.112.79      15:15   Strumica   1800
+
+1
+Jovan Todorov    1000    10.73.112.200     16:30   Bitola     760
+
+Result:
+IP network: 10.73.112 has the following number of users:
+2
+The user who logged on earliest after noon from that network is:
+Viktor Jovev with salary 2200 from address 10.73.112.79 who logged in at 15:15
+*/
+
 package Labs.Lab06_HashTables;
 
 import java.util.Scanner;
