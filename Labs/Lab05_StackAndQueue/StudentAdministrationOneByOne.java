cd8c564c0fc6056eb9707628683d099a342e2576
Sandra Chavdarova <148618364+sandra-chavdarova@users.noreply.github.com>
2025-01-26 19:10:41 +0100

    Update and rename StudentAdministrationParallel.java to StudentAdministrationOneByOne.java

--- Labs/Lab05_StackAndQueue/StudentAdministrationOneByOne.java
+++ Labs/Lab05_StackAndQueue/StudentAdministrationOneByOne.java
@@ -98,7 +98,7 @@ class Student {
     }
 }
 
-public class StudentAdministrationParallel {
+public class StudentAdministrationOneByOne {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
